4ae2af031493c25e4d7f6bd1154625f921553334
Benitez Araceli <benitezaraceli@gmail.com>
2018-03-08 12:49:11 -0300

    Mock

---
+++ IntentoMock.java
@@ -0,0 +1,11 @@
+package com.lmj;
+
+import org.json.*;
+
+public class MainMock {
+
+	public static void main(String[] args) {
+	
+		String client = "{\"clientID\":null,\"type\":\"text\",\"Account\":{\"accountID\":null,\"balance\":null},\"Person\":{\"name\":\"text\",\"surname\":\"text\",\"dni\":1234,\"birthday\":\"text\",\"nationality\":\"text\",\"contactInformation\":{\"phone\":\"text\",\"mobilePhone\":\"text\",\"email\":\"text\",\"locationInformation\":{\"address\":\"text\",\"zipcode\":\"text\",\"city\":\"text\",\"province\":\"text\",\"country\":\"text\"}}}}";
+	}
+}
--- Validations.java
+++ Validations.java
@@ -11,73 +11,71 @@ public class Validations {
     public static boolean clientValidation (JSONObject client) {
     
 		if(!client.has("clientId") || !client.has("type") || !client.has("account") || !client.has("person")) {
-			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
 			return false;
 		} else {
 			JSONObject account = new JSONObject(request.getParameter("account"));
 			JSONObject account = new JSONObject(request.getParameter("person"));
 			accountValidation(account);
 			personValidation(person);
+			
+			if( !accountValidation && !personValidation == true) {
+                return false;
+            } else {
+                return true;
+			}
 		}
-		//Aca van los ifs en cascada
 	}
 	
 	public void accountValidation (JSONObject account) {
 		
 		if(!account.has("accountId") || (!account.has("balance")) {
-			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return false;
 		} else {
-			String accountId = account.getString("accountId");
-			String balance = account.getString("balance");
+			return true;
 		}
 	}
             
-	public void personValidation (JSONObject person){
+	public static boolean personValidation (JSONObject person){
             
 		if(!person.has("name") || !person.has("surname") || !person.has("dni") || !person.has("birthday") 
 		|| !person.has("nationality") || !person.has("contactInformation")) {
-			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return false;
 		} else {
-			String name = person.getString("name");
-			String surname = person.getString("surname");
-			String dni = person.getString("dni");
-			String birthday = person.getString("birthday");
-			String nationality = person.getString("nationality");
-			
 			JSONObject contactInformation = new JSONObject(request.getParameter("contactInformation"));
 			contactInformationValidation(contactInformation);
+			
+			if( !contactInformationValidation == true) {
+                return false;
+            } else {
+                return true;
+			}
 		}
 	}
 	
-	public void contactInformationValidation (JSONObject contactInformation) {
+	public static boolean contactInformationValidation (JSONObject contactInformation) {
             
 		if(!contactInformation.has("phone") || !contactInformation.has("mobilePhone") || !contactInformation.has("email")
 		 || !contactInformation.has("locationInformation")) {
-			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return false;
 		} else {
-			String phone = contactInformation.getString("phone");
-			String mobilePhone = contactInformation.getString("mobilePhone");
-			String email = contactInformation.getString("email");
-			
 			JSONObject locationInformation = new JSONObject(request.getParameter("locationInformation"));
 			locationInformationValidation(locationInformation);
+			
+			if( !locationInformationValidation == true) {
+                return false;
+            } else {
+                return true;
+			}
 		}
 	}
 	
-	public void locationInformationValidation (JSONObject locationInformation) {
-		
-		JSONObject locationInformationStatus = new JSONObject();
+	public static boolean locationInformationValidation (JSONObject locationInformation) {
 		
 		if(!locationInformation.has("address") || !locationInformation.has("zipcode") 
 			|| !locationInformation.has("province") || !locationInformation.has("country")) {
-			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-			locationInformationStatus.put(false);
+			return false;
 		} else {
-			String address = locationInformation.getString("address");
-			String zipcode = locationInformation.getString("zipcode");
-			String province = locationInformation.getString("province");
-			String country = locationInformation.getString("country");
-            locationInformationStatus.put(true);
+            return true;
 		}
 	}
 }
